6144bef21bdd17a0702df48174599614f03cd8ba
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-27 10:09:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,52 @@
+#main.tf
+
+data "http" "aksc_release" {
+  url = "https://github.com/Azure/AKS-Construction/releases/download/0.9.8/main.json"
+  request_headers = {
+    Accept = "application/json"
+    User-Agent = "request module"
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name = var.resourceGroupName
+  location = var.location
+}
+
+resource "azurerm_resource_group_template_deployment" "aksc_deploy" {
+  name = "AKS-C"
+  resource_group_name = azurerm_resource_group.rg.name
+  deployment_mode = "Incremental"
+  template_content = data.http.aksc_release.body
+  parameters_content = jsonencode({
+    resourceName = {value=var.resourceName}
+    agentCount = {value=var.agentCount}
+    agentCountMax = {value=var.agentCountMax}
+    custom_vnet = {value=var.custom_vnet}
+    bastion = {value=var.bastion}
+    enable_aad = {value=var.enable_aad}
+    AksDisableLocalAccounts = {value=var.AksDisableLocalAccounts}
+    enableAzureRBAC = {value=var.enableAzureRBAC}
+    adminPrincipalId = {value=data.azurerm_client_config.current.client_id}
+    registries_sku = {value=var.registries_sku}
+    acrPushRolePrincipalId = {value=data.azurerm_client_config.current.client_id}
+    privateLinks = {value=var.privateLinks}
+    keyVaultIPAllowlist = {value=var.keyVaultIPAllowlist}
+    omsagent = {value=var.omsagent}
+    retentionInDays = {value=var.retentionInDays}
+    networkPolicy = {value=var.networkPolicy}
+    azurepolicy = {value=var.azurepolicy}
+    enablePrivateCluster = {value=var.enablePrivateCluster}
+    ingressApplicationGateway = {value=var.ingressApplicationGateway}
+    appGWcount = {value=var.appGWcount}
+    appGWsku = {value=var.appGWsku}
+    appGWmaxCount = {value=var.appGWmaxCount}
+    appgwKVIntegration = {value=var.appgwKVIntegration}
+    keyVaultAksCSI = {value=var.keyVaultAksCSI}
+    keyVaultCreate = {value=var.keyVaultCreate}
+    keyVaultOfficerRolePrincipalId = {value=data.azurerm_client_config.current.client_id}
+    acrPrivatePool = {value=var.acrPrivatePool}
+  })
+}
