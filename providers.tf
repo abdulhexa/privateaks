2fc8edb1ca8ba5116f6ad31381501befd140de73
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-27 10:29:49 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,19 @@
 terraform {
-  required_version = ">=1.1.9"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>3.6"
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
+  }
+
+  backend "azurerm" {
+    key              = "terraform.tfstate"
+    use_oidc         = true
+    use_azuread_auth = true
   }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
